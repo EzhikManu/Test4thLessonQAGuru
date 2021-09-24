581250b092846c683823c17a7056b921d4c6aa53
EzhikManu <iljasstan@mail.ru>
2021-09-24 18:54:21 +0300

    Passed Tests

---
+++ src/test/java/com/iljasstan/IsJUnit5ExampleOnSoftAssertions.java
@@ -0,0 +1,20 @@
+package com.iljasstan;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class IsJUnit5ExampleOnSoftAssertions {
+    @Test
+    void shouldFindJUnit5Example() {
+        Configuration.browser = "opera";
+        Configuration.startMaximized = true;
+
+        open("https://github.com/selenide/selenide/wiki/SoftAssertions");
+        $("#wiki-body").shouldHave(text("JUnit5"));
+
+    }
+}
