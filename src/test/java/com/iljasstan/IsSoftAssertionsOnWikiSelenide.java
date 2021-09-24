c739b3f2ad96cd85b2a15cec3c57b99ccba6dd57
Ezhik_Manu <iljastan@mail.ru>
2021-09-24 15:14:59 +0300

    first half of homework 24/09/21

---
+++ src/test/java/com/iljasstan/IsSoftAssertionsOnWikiSelenide.java
@@ -0,0 +1,23 @@
+package com.iljasstan;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selenide.$;
+
+public class IsSoftAssertionsOnWikiSelenide {
+    @Test
+    void isSoftAssertionsOnWikisPage() {
+        Configuration.browser = "opera";
+        Configuration.startMaximized = true;
+        open("https://github.com");
+        $("[data-test-selector='nav-search-input']").setValue("Selenide").pressEnter();
+        $$(".repo-list li").first().$("a").pressEnter();
+        $("#wiki-tab").click();
+
+        $("#wiki-pages-box").$("ul").$("li button").click();
+        $("#wiki-pages-box").$("ul").shouldHave(Condition.text("SoftAssertions"));
+    }
+}
